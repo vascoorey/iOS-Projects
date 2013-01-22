047e867d69cc585fbf2e82c36817daada151b12f
Vasco Orey <vasco.orey@gmail.com>
2013-01-22 14:56:48 +0000

    Starting to add functionality. Still very simple.

--- Checklists/Checklists/ChecklistsViewController.h
+++ Checklists/Checklists/ChecklistsViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ChecklistsViewController : UIViewController
+@interface ChecklistsViewController : UITableViewController
 
 @end
--- Checklists/Checklists/ChecklistsViewController.m
+++ Checklists/Checklists/ChecklistsViewController.m
@@ -9,10 +9,22 @@
 #import "ChecklistsViewController.h"
 
 @interface ChecklistsViewController ()
-
+@property (nonatomic, strong) NSMutableArray *rows;
 @end
 
 @implementation ChecklistsViewController
+
+@synthesize rows = _rows;
+
+-(NSMutableArray *)rows
+{
+    if(!_rows)
+    {
+        _rows = [NSMutableArray arrayWithObjects:@"Walk the dog", @"Brush my teeth", @"Learn iOS development...", @"Soccer practice", @"Eat ice cream", nil];
+    }
+    // Returns an immutable copy
+    return [_rows copy];
+}
 
 - (void)viewDidLoad
 {
@@ -29,6 +41,20 @@
 -(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
 {
     return toInterfaceOrientation == UIInterfaceOrientationPortrait;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.rows count] * 50;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    // Rethink this bit... Use introspection to guard !
+    UILabel *cellLabel = (UILabel *)[cell viewWithTag:1000];
+    cellLabel.text = (NSString *)[self.rows objectAtIndex:indexPath.row % 5];
+    return cell;
 }
 
 @end
