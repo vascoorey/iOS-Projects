3c3700db739827d3f97bbd4135ed8555dba7e9b8
Vasco Orey <vasco.orey@gmail.com>
2013-02-25 12:23:09 +0000

    Started Homework 4: SPoT

---
+++ SPoT/SPoT/StanfordPhotosTVC.h
@@ -0,0 +1,13 @@
+//
+//  StanfordPhotosTVC.h
+//  SPoT
+//
+//  Created by Vasco Orey on 2/25/13.
+//  Copyright (c) 2013 Delta Dog Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StanfordPhotosTVC : FlickrPhotoTVC
+
+@end
---
+++ SPoT/SPoT/StanfordPhotosTVC.m
@@ -0,0 +1,13 @@
+//
+//  StanfordPhotosTVC.m
+//  SPoT
+//
+//  Created by Vasco Orey on 2/25/13.
+//  Copyright (c) 2013 Delta Dog Studios. All rights reserved.
+//
+
+#import "StanfordPhotosTVC.h"
+
+@implementation StanfordPhotosTVC
+
+@end
