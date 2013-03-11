578ca67a611dbc49607b64624a41e6015d9c77e7
Vasco Orey <vasco.orey@gmail.com>
2013-03-11 10:08:38 +0000

    Started adding Photomania

---
+++ Photomania/Photomania/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photomania
+//
+//  Created by Vasco Orey on 3/11/13.
+//  Copyright (c) 2013 Delta Dog Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
