d2907ebece15c53529bcb9b717573c2f93f1b67f
Vasco Orey <vasco.orey@gmail.com>
2013-03-22 11:15:46 +0000

    Added some gameplay

--- Kitchen-Sink/Kitchen-Sink/KitchenSinkViewController.m
+++ Kitchen-Sink/Kitchen-Sink/KitchenSinkViewController.m
@@ -12,22 +12,38 @@
 @interface KitchenSinkViewController ()
 @property (weak, nonatomic) IBOutlet UIView *kitchenSink;
 @property (weak, nonatomic) NSTimer *drainTimer;
+@property (weak, nonatomic) NSTimer *gameTimer;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *scoreButtonItem;
+@property (nonatomic) NSUInteger score;
 @end
 
 @implementation KitchenSinkViewController
 
-#define MOVE_DURATION 2.00f
-#define DRAIN_DURATION 2.00f
-#define DRAIN_DELAY 0.00f
+#define MOVE_DURATION 2.0f
+#define DRAIN_DURATION 2.0f
+#define DRAIN_DELAY 0.0f
+#define DISH_CLEANING_INTERVAL 2.0f
+#define GAME_DURATION 30.0f
+
+-(void)cleanDish
+{
+    if(self.kitchenSink.window)
+    {
+        [self addFood:nil];
+        [self performSelector:@selector(cleanDish) withObject:nil afterDelay:DISH_CLEANING_INTERVAL];
+    }
+}
 
 -(void)startDrainTimer
 {
     self.drainTimer = [NSTimer scheduledTimerWithTimeInterval:DRAIN_DURATION/3 target:self selector:@selector(drain:) userInfo:nil repeats:YES];
+    self.gameTimer = [NSTimer scheduledTimerWithTimeInterval:GAME_DURATION target:self selector:@selector(gameOver:) userInfo:nil repeats:NO];
 }
 
 -(void)stopDrainTimer
 {
     [self.drainTimer invalidate];
+    [self.gameTimer invalidate];
     self.drainTimer = nil;
 }
 
@@ -35,12 +51,19 @@
 {
     [super viewDidAppear:animated];
     [self startDrainTimer];
+    [self cleanDish];
 }
 
 -(void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
     [self stopDrainTimer];
+}
+
+- (IBAction)restart:(id)sender {
+    self.scoreButtonItem.title = @"Points: 0";
+    self.score = 0;
+    [self startDrainTimer];
 }
 
 - (IBAction)tap:(UITapGestureRecognizer *)sender {
@@ -66,7 +89,17 @@
 -(void)drain:(NSTimer *)timer
 {
     [self drain];
-    NSLog(@"Draining...");
+}
+
+-(void)gameOver:(NSTimer *)timer
+{
+    [self stopDrainTimer];
+    for(UIView *view in self.kitchenSink.subviews)
+    {
+        [view removeFromSuperview];
+    }
+    NSString *score = [NSString stringWithFormat:@"Way to go champ! You only lost %d foods!", self.score];
+    [[[UIAlertView alloc] initWithTitle:@"Game Over!" message:score delegate:nil cancelButtonTitle:@"OK!" otherButtonTitles:nil, nil] show];
 }
 
 -(IBAction)drain
@@ -103,6 +136,8 @@
                                                                                completion:^(BOOL success){
                                                                                    if(success)
                                                                                    {
+                                                                                       self.score ++;
+                                                                                       self.scoreButtonItem.title = [NSString stringWithFormat:@"Points: %d", self.score];
                                                                                        [view removeFromSuperview];
                                                                                    }
                                                                                }];
@@ -117,6 +152,31 @@
 -(void)addFood:(NSString *)food
 {
     UILabel *foodLabel = [[UILabel alloc] init];
+    
+#define BLUE_FOOD @"Jello"
+#define GREEN_FOOD @"Broccolli"
+#define ORANGE_FOOD @"Carrot"
+#define RED_FOOD @"Red Pepper"
+#define PURPLE_FOOD @"Eggplant"
+#define BROWN_FOOD @"Potato Peels"
+    
+    static NSDictionary *foods = nil;
+    if(!foods)
+    {
+        foods = @{ BLUE_FOOD : [UIColor blueColor],
+                   GREEN_FOOD : [UIColor greenColor],
+                   ORANGE_FOOD : [UIColor orangeColor],
+                   RED_FOOD : [UIColor redColor],
+                   PURPLE_FOOD : [UIColor purpleColor],
+                   BROWN_FOOD : [UIColor brownColor] };
+    }
+    
+    if(![food length])
+    {
+        food = [[foods allKeys] objectAtIndex:arc4random() % [foods count]];
+        foodLabel.textColor = foods[food];
+    }
+    
     foodLabel.text = food;
     foodLabel.font = [UIFont systemFontOfSize:46];
     foodLabel.backgroundColor = [UIColor clearColor];
